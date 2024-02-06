7b452fbacbdf769be7eb86884a418f3d5032bc03
brian <bfkenny@gmail.com>
2024-02-06 15:08:23 +0000

    Day 1

--- Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days/src/main.rs
+++ Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days/src/main.rs
@@ -1,27 +1,28 @@
 use std::io;
 
+const NUM_DAYS_IN_YEAR: u32 = 365;
+
 fn main() {
+    let guess = get_input();
+    let age: u32 = calculate_age(&guess);
+    println!("You are roughly {age} days old!");
+}
 
+fn get_input() -> u32 {
     loop {
-
-    println!("Please input your age in years!");
-
-    let mut guess = String::new();
-
-    
+        println!("Please input your age in years:");
+        let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
-            .expect("Could not read age. Make you an integer is used.");
-        
-        let guess: u32 =  match guess.trim().parse() {
+            .expect("Could not read age. Make sure an integer is used.");
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num, 
             Err(_) => continue,
         };
-
-        let age: u32 = guess * 365;
-
-        println!("You are roughly {age} days old!")
-
+        return guess;
     }
+}
 
+fn calculate_age(num: &u32) -> u32 {
+    num * NUM_DAYS_IN_YEAR
 }
